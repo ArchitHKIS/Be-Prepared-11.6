f7d9370e29d1d996f6c48497284e8e460640f9bf
“Archit <190902@hkis.edu.hk>
2017-03-16 08:26:12 +0800

    idk
    halp

--- DocumentIndex.java
+++ DocumentIndex.java
@@ -13,24 +13,31 @@ public class DocumentIndex extends ArrayList<IndexEntry> {
 	}
 
 	public void addWord(String word, int num) {
-		int place = foundOrInserted(word);
-		IndexEntry dd = super.get(place);
-		dd.add(num);
-		
+		get(foundOrInserted(word)).add(num);
 	}
 
 	public void addAllWords(String str, int num) {
-		String[] array = str.split("\\W+");
-		for(String string : array){
-			if(string.length() > 0){
-				addWord(string, num);
+		String[] strArray = str.split("[., \"!?]"); // Splits the array except
+													// for blank lines
+		for (String s : strArray) // For every String sorted
+			if (!s.equals("")) // If it is not an empty line
+				addWord(s, num); // Add this to the num position
+	}
+
+	private int foundOrInserted(String word) {
+		int num;
+		int num1;
+		for (num = 0; num < super.size(); num++) {
+			num1 = get(num).getWord().compareToIgnoreCase(word);
+			System.out.println(get(num).getWord().compareToIgnoreCase(word));
+			if (num1 == 0) {
+				return num;
+			} else if (num1 > 0) {
+				break;
 			}
 		}
-
-	}
-	private int foundOrInserted(String word){
-	
-		return 0;	
+		super.add(num, new IndexEntry(word));
+		return num;
 	}
 
 }
--- IndexEntry.java
+++ IndexEntry.java
@@ -5,24 +5,33 @@ public class IndexEntry {
 	private static ArrayList<Integer> numList;
 
 	public IndexEntry(String w){
-		word = w.toUpperCase();
-		numList = new ArrayList<Integer>();
+		word = w.toUpperCase(); // makes the word an uppercase string only
+		numList = new ArrayList<Integer>(); //initializes the numlist array as an integer array
 	}
 	
 	public static void add(int num) {
-		if(numList.contains(num) == false){
-			numList.add(num);
-		}
+		Integer num1 = num;
+        if (!numList.contains( num1 ) )
+        {
+            numList.add( num1 );
+        }
 	}
 
 	public static String getWord() {
-		return word;
+		return word; // returns private var word 
 	}
 
 	public String toString() {
-		String index = word + numList.toString();
-		index.replace("[", "");
-		return index;
+	/*	String index = word + " " + numList.toString(); // makes a string with word followed by numlist as a string
+		index.replace("[", ""); // removes the square brackets
+		return index; // return statement*/
+		String result = word;
+        for ( Integer num : numList )
+        {
+            result += ( " " + num + ",");
+        }
+        result = result.substring( 0 , result.length() - 1 );
+        return result;
 	}
 
 }
